6262492d86179abfdebfaf97084cf25bd69591b5
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2022-05-20 19:39:01 +0300

    Show card creation form by request

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CardCreationForm } from '../CardCreationForm/CardCreationForm';
 import styles from './CardList.module.css';
 
 interface ICardListProps {
@@ -7,27 +8,43 @@ interface ICardListProps {
 }
 
 export function CardList({ children, title }: ICardListProps) {
+  const [isFormShown, setFormShown] = React.useState(false);
+
+  const handleShowFormClick = () => {
+    setFormShown(true);
+  };
+
+  const handleHideFormClick = () => {
+    setFormShown(false);
+  };
+
   return (
     <div className={styles.cardList}>
       <header className={styles.cardListHeader}>{title}</header>
       <main className={styles.cardListBody}>{children}</main>
       <footer className={styles.cardListFooter}>
-        <button className={styles.cardListCreateButton} type="button">
-          <svg
-            className={styles.cardListCreateButtonIcon}
-            viewBox="0 0 24 24"
-            width="18"
-            height="18"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <line x1="12" y1="5" x2="12" y2="19"></line>
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-          </svg>
-          <span className={styles.cardListCreateButtonIcon}>Add card</span>
-        </button>
+        {isFormShown && <CardCreationForm onHideFormClick={handleHideFormClick} />}
+        {!isFormShown && (
+          <button
+            className={styles.cardListCreateButton}
+            onClick={handleShowFormClick}
+            type="button">
+            <svg
+              className={styles.cardListCreateButtonIcon}
+              viewBox="0 0 24 24"
+              width="18"
+              height="18"
+              stroke="currentColor"
+              strokeWidth="2"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round">
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+            <span className={styles.cardListCreateButtonText}>Add card</span>
+          </button>
+        )}
       </footer>
     </div>
   );
