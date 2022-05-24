a34d909ad539de82a2d094258e45d304feaae34c
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2022-05-24 18:49:39 +0300

    Remove unused imports

--- src/views/Board/BoardPage.tsx
+++ src/views/Board/BoardPage.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import {
-  DndContext,
-  DragEndEvent,
-  KeyboardSensor,
-  MouseSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Board } from '../../components/Board/Board';
 import { CardList } from '../../components/CardList/CardList';
 import { selectCardLists } from '../../store/board/board.slice';
-import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
-import { moveCard } from '../../store/cards/cards.slice';
 
 export function BoardPage() {
   const cardLists = useSelector(selectCardLists);
